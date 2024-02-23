9867eb2545b393ea90bba7de24cb63d7a6b9fc2a
Arthur <arthur0139@gmail.com>
2024-02-23 12:17:58 -0300

    *

--- programs/vendor.go
+++ programs/vendor.go
@@ -17,22 +17,20 @@ func setupGraphs() *dt.Graph {
 	graph := &dt.Graph{}
 
 	// Add some nodes
-	node1 := graph.AddNode(&Vendor{Name: "Maria", Ocuppation: "Vendedora de manga"})
-	node2 := graph.AddNode(&Vendor{Name: "João", Ocuppation: "Vendedor de mamão"})
-	node3 := graph.AddNode(&Vendor{Name: "José", Ocuppation: "Vendedor de abacate"})
-	node4 := graph.AddNode(&Vendor{Name: "Helena", Ocuppation: "Vendedora de abacaxi"})
-	graph.AddNode(&Vendor{Name: "Alice", Ocuppation: "Vendedora de abacaxi"})
-	node6 := graph.AddNode(&Vendor{Name: "Bob", Ocuppation: "Vendedor de abacate"})
-	node7 := graph.AddNode(&Vendor{Name: "Carmem", Ocuppation: "Vendedora de mamão"})
+	node1 := graph.AddNode(&Vendor{Name: "Alice", Ocuppation: "Vendedora de abacaxi"})
+	node2 := graph.AddNode(&Vendor{Name: "Bob", Ocuppation: "Vendedor de mamão"})
+	node3 := graph.AddNode(&Vendor{Name: "Peggy", Ocuppation: "Vendedor de abacate"})
+	node4 := graph.AddNode(&Vendor{Name: "Claire", Ocuppation: "Vendedora de abacaxi"})
+	node5 := graph.AddNode(&Vendor{Name: "Anuj", Ocuppation: "Vendedora de mamão"})
+	node6 := graph.AddNode(&Vendor{Name: "Thom", Ocuppation: "Vendedora de manga"})
+	node7 := graph.AddNode(&Vendor{Name: "Jonny", Ocuppation: "Vendedora de mamão"})
 
 	// Add edges between nodes
-	graph.AddEdge(node1, node2)
+	graph.AddEdge(node2, node5)
+	graph.AddEdge(node2, node3)
 	graph.AddEdge(node1, node3)
-	graph.AddEdge(node2, node4)
-	graph.AddEdge(node3, node4)
-	graph.AddEdge(node7, node2)
-	graph.AddEdge(node6, node7)
-	graph.AddEdge(node3, node1)
+	graph.AddEdge(node4, node6)
+	graph.AddEdge(node4, node7)
 
 	return graph
 }
@@ -40,15 +38,15 @@ func setupGraphs() *dt.Graph {
 func setupQueue(graph *dt.Graph) *dt.Queue {
 	queue := dt.Queue{}
 
-	queue.Push(graph.Nodes[2])
-	queue.Push(graph.Nodes[4])
-	queue.Push(graph.Nodes[5])
+	queue.Push(graph.Nodes[0])
+	queue.Push(graph.Nodes[1])
+	queue.Push(graph.Nodes[3])
 
 	return &queue
 }
 
-func executeQueue(queue *dt.Queue) []string {
-	var path []string
+func executeQueue(queue *dt.Queue) []*Vendor {
+	var path []*Vendor
 
 	current := queue.Rear()
 
@@ -57,12 +55,12 @@ func executeQueue(queue *dt.Queue) []string {
 		node := current.Data.(*dt.GraphNode)
 		vendor := node.Data.(*Vendor)
 
-		if contains(path, vendor.Name) {
+		if contains(path, vendor) {
 			current = current.Next
 			continue
 		}
 
-		path = append(path, vendor.Name)
+		path = append(path, vendor)
 
 		if strings.Contains(vendor.Ocuppation, "manga") {
 			break
@@ -80,10 +78,9 @@ func executeQueue(queue *dt.Queue) []string {
 	return path
 }
 
-
-func contains(list []string, item string) bool {
+func contains(list []*Vendor, item *Vendor) bool {
 	for _, i := range list {
-		if i == item {
+		if i.Name == item.Name && i.Ocuppation == item.Ocuppation {
 			return true
 		}
 	}
@@ -93,5 +90,12 @@ func contains(list []string, item string) bool {
 func RunVendor() {
 	graph := setupGraphs()
 	queue := setupQueue(graph)
-	fmt.Println(executeQueue(queue))
+
+	path := executeQueue(queue)
+
+	for i := 0; i < len(path); i++ {
+		fmt.Println(path[i].Name)
+		fmt.Println(path[i].Ocuppation)
+		fmt.Println("--------------------------")
+	}
 }
